66130f52e5e25bd04f030dc206541354cfe131f1
chamodi0811 <chamodik14@gmail.com>
2022-04-30 19:14:43 +0530

    Added POS system

---
+++ Layered_Architecture/src/db/DBConnection.java
@@ -0,0 +1,29 @@
+package db;
+
+import com.mysql.jdbc.Connection;
+
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author : Sanu Vithanage
+ * @since : 0.1.0
+ **/
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private final Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "1234");
+    }
+
+    public static DBConnection getDbConnection() throws SQLException, ClassNotFoundException {
+        return dbConnection == null ? dbConnection= new DBConnection() : dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
